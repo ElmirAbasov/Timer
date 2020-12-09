e9aaef3df1b85981203044856aee8a20214dde15
Xizafren <m-ett@hotmail.se>
2020-12-09 14:32:16 +0100

    Initial Commit

---
+++ Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by Elmir Abasov on 2020-12-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
